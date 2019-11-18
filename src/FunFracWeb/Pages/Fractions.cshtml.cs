d44a8f155c8788c8a74b6a094ed3429c2403a448
Tyler Jensen <tyler.jensen@ioka.us>
2019-11-18 14:32:49 -0700

    Simplistic web UI for fractions calculator.

---
+++ src/FunFracWeb/Pages/Fractions.cshtml.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using FracFunLib;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.Extensions.Logging;
+
+namespace FunFracWeb.Pages
+{
+    public class FractionsModel : PageModel
+    {
+        private readonly ILogger<FractionsModel> _logger;
+
+        public string FractionInput { get; set; }
+        public string ResultMessage { get; set; }
+
+        public FractionsModel(ILogger<FractionsModel> logger)
+        {
+            _logger = logger;
+        }
+
+        public void OnGet()
+        {
+            FractionInput = "Enter your fraction equation here.";
+            ResultMessage = string.Empty;
+        }
+
+        public void OnPost()
+        {
+            try
+            {
+                FractionInput = Request.Form[nameof(FractionInput)];
+
+                IParser parser = new Parser();
+                ICalculator calculator = new Calculator(parser);
+                var result = calculator.Calculate(FractionInput);
+                ResultMessage = $"The answer is: {result}";
+            }
+            catch (Exception e)
+            {
+                ResultMessage = e.Message;
+            }
+        }
+    }
+}
